804bca0e036766a0a548553a5b45f5dfe57ad15c
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-05-15 13:49:09 +0200

    Code Cleanup: Unterdrücken von Warnings die für das Demoprojekt okay sind

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Main {
+    @SuppressWarnings("java:S106")// Okay da Demoprojekt
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
