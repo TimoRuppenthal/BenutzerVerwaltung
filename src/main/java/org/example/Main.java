a7b17d082f49f1fa4d97e2f5857496f264591dfa
JeremiePlanta <Jeremie.Planta@stahl-holding-saar.de>
2023-09-07 09:08:05 +0200

    Umstellen der Main Methode so dass der Outbound Port (VerifizierungsCodeSender)sich für DomainEvents registriert ohne VermittlerKlasse

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,11 @@ package org.example;
 import io.jexxa.core.JexxaMain;
 import io.jexxa.drivingadapter.rest.RESTfulRPCAdapter;
 import org.example.applicationservice.BenutzerVerwaltung;
-import org.example.domainservice.DomainEventSender;
+import org.example.domain.VerifizierungsCodeVerschickt;
 import org.example.domainservice.StammdatenService;
+import org.example.domainservice.VerifizierungsCodeSender;
+
+import static org.example.domain.DomainEventPublisher.subscribe;
 
 public class Main {
     @SuppressWarnings("java:S106")// Okay da Demoprojekt
@@ -14,7 +17,7 @@ public class Main {
 
         jexxaMain
                 .bootstrap(StammdatenService.class).with(StammdatenService::initStammdaten)
-                .bootstrap(DomainEventSender.class).and()
+                .bootstrap(VerifizierungsCodeSender.class).with(sender -> subscribe(VerifizierungsCodeVerschickt.class, sender::send))
                 // Bind a REST adapter to expose parts of the application
                 .bind(RESTfulRPCAdapter.class).to(BenutzerVerwaltung.class)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
                 .bind(RESTfulRPCAdapter.class).to(jexxaMain.getBoundedContext())  // Get stats: http://localhost:7501/BoundedContext/isRunning
